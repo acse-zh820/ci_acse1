0697164ed19e5c7a46e98d1785002c06abfcf24e
acse-zh820 <45823087+acse-zh820@users.noreply.github.com>
2020-10-21 18:18:30 +0800

    commit

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,9 +1,12 @@
-
-__all__ = ['my_sum']
-
+from functools import lru_cache
+__all__ = ['my_sum','factorial']
 
 def my_sum(iterable):
     tot = 0
     for i in iterable:
         tot += i
     return tot
+
+@lru_cache(maxsize=None)
+def factorial(n):
+    return n*factorial(n-1) if n else 1
